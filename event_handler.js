dfb9fc535a376b74e1c12c175f8f0a8c51b111c4
BMayowa <mayowaobi74@gmail.com>
2023-01-07 16:14:52 +0100

    Summary Search Type added, plus updates and enhancements

--- event_handler.js
+++ event_handler.js
@@ -12,11 +12,23 @@ export class EventHandler {
         /* URL FIELD FOCUS IN EVENT */
         _('#id-url-field').addEventListener('focusin', function() {
             this.setAttribute('placeholder', 'https://')
-            this.value = "https://"
+            this.value.trim() === "" ? this.value = "https://" : null
         })
         _('#id-url-field').addEventListener('focusout', function() {
             // this.value = ''
             this.setAttribute('placeholder', 'Type or paste url to summarize')
         })
+        _('[data-summary_field_trigger="url"]').addEventListener('click', function() {
+            new SummaryForm(this).activateURLSummaryField()
+        })
+        _('[data-summary_field_trigger="text"]').addEventListener('click', function() {
+            new SummaryForm(this).activateTextSummaryField()
+        })
+        _('[data-summary_field_trigger="search"]').addEventListener('click', function() {
+            new SummaryForm(this).activateSearchSummaryField()
+        })
+        // _('[data-summary_field_trigger="news"]').addEventListener('click', function() {
+        //     new SummaryForm(this).activateNewsSummaryField()
+        // })
     }
 }
